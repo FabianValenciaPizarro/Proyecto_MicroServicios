563fe10bbc2f6f71332f1bfb1504fdd5aa0a1334
chrNotFound <chr.troncoso@duocuc.cl>
2026-05-21 16:35:46 -0400

    Delete payment-service directory

--- payment-service/src/main/resources/db/migration/V1__crear_tabla_pagos.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE pagos (
-                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                       pedido_id BIGINT NOT NULL,
-                       metodo_pago VARCHAR(50) NOT NULL,
-                       monto DOUBLE NOT NULL,
-                       estado VARCHAR(20) NOT NULL DEFAULT 'PENDIENTE',
-                       transaccion_id VARCHAR(100),
-                       fecha_pago TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
