653f8fd5acc57a22f9da4230e8b8720b324196d3
chrNotFound <chr.troncoso@duocuc.cl>
2026-05-21 14:54:27 -0400

    Delete catalog-service directory

--- catalog-service/target/classes/db/migration/V1__crear_tabla_materiales.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE materiales (
-                            id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                            nombre_material VARCHAR(100) NOT NULL,
-                            precio_por_cm2 DOUBLE NOT NULL,
-                            stock_actual DOUBLE NOT NULL,
-                            descripcion VARCHAR(255),
-                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
